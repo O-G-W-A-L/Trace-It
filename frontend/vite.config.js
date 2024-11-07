6d0681172fd91dc28db506f1d3d5bb204a7677f4
O-G-W-A-L <ogwalja04@gmail.com>
2024-11-07 13:15:08 +0300

    github

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Trace-It/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
