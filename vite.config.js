9866433573122695ff3baf9fa10ff0e8708d5580
O-G-W-A-L <ogwalja04@gmail.com>
2024-12-30 14:35:43 +0300

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/Trace-It/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
