22c1def7a936365ffa4e8ab258abfb50b7f6455a
O-G-W-A-L <ogwalja04@gmail.com>
2024-12-30 20:57:00 +0300

    fix backend

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,5 @@ from app import create_app
 # Entry point for the Flask app
 app = create_app()
 
-# Adding a simple test route
-@app.route('/api/test', methods=['GET'])
-def test():
-    return "Test route is working!"
-
 if __name__ == "__main__":
     app.run(debug=True)
